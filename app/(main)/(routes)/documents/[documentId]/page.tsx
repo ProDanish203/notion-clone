667d5d7d7f81570ee4e94ad99a7f189c1ac6d8e2
Danish <danishsidd524@gmail.com>
2025-03-09 00:13:08 +0500

    add: icon picker and toolbar ui

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+"use client";
+
+import { Toolbar } from "@/components/toolbar";
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams } from "next/navigation";
 
 const SingleDocumentPage = () => {
-  return <div>singleDocumentPage</div>;
+  const params = useParams<{ documentId: Id<"documents"> }>();
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-2-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default SingleDocumentPage;
---
+++ components/icon-picker.tsx
@@ -0,0 +1,36 @@
+"use client";
+import EmojiPicker, { Theme } from "emoji-picker-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useTheme } from "next-themes";
+
+interface IconPickerProps {
+  onChange: (icon: string) => void;
+  children: React.ReactNode;
+  asChild?: boolean;
+}
+
+export const IconPicker = ({
+  onChange,
+  children,
+  asChild,
+}: IconPickerProps) => {
+  const { resolvedTheme } = useTheme();
+  const theme = resolvedTheme === "dark" ? Theme.DARK : Theme.LIGHT;
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild={asChild}>{children}</PopoverTrigger>
+      <PopoverContent className="p-0 w-full border-none shadow-none">
+        <EmojiPicker
+          height={350}
+          theme={theme}
+          onEmojiClick={(data) => onChange(data.emoji)}
+        />
+      </PopoverContent>
+    </Popover>
+  );
+};
